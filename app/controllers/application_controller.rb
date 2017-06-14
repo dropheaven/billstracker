183cea2c7cca91347a9136b61b7c375d64aa72e7
Saad <Dropheaven@live.com>
2017-06-14 13:55:06 -0400

    Added basic structure to application

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+    "Hello World"
+  end
+
+end
