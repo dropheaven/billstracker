f7f9c117d8b1a3ee6cb134f2ab298c378eece828
Saad <Dropheaven@live.com>
2017-06-14 15:43:03 -0400

    added signup route and form page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,16 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      redirect '/bills'
+    else
+      erb :'/users/create_user'
+    end
+  end
+
+  post '/signup' do
+    
+  end
+
+
 end
