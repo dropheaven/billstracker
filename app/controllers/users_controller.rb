bcda6ae3dfad6d36287d125504283e17b749b2e5
Saad <Dropheaven@live.com>
2017-06-15 07:36:29 -0400

    login and logout routes created

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,39 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    
+    @user = User.create(params)
+    if !@user.valid?
+      redirect '/signup'
+    else
+      redirect '/bills'
+    end
   end
 
+  get '/login' do
+    if logged_in?
+      redirect '/bills'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/bills"
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 
 end
