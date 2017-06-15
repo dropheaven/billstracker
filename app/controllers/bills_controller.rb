436349ade849d751f21c8e700abe0e90d35e58af
Saad <Dropheaven@live.com>
2017-06-15 07:31:46 -0400

    create and read routes added

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,3 +1,40 @@
 class BillsController < ApplicationController
 
+  get '/bills' do
+    if logged_in?
+      @user = current_user
+      erb :'/bills/index'
+    else
+      redirect '/'
+    end
+  end
+
+  get '/bills/new' do
+    if logged_in?
+      erb :'/bills/new'
+    else
+      redirect '/'
+    end
+  end
+
+  post '/bills' do
+    @bill = Bill.create(params)
+    if @bill.valid?
+      user = current_user
+      user.bills << @bill
+      redirect :"/bills/#{@bill.id}"
+    else
+      redirect '/bills/new'
+    end
+  end
+
+  get '/bills/:id' do
+    if logged_in?
+      @bill = Bill.find(params[:id])
+      erb :'/bills/show'
+    else
+      redirect '/'
+    end
+  end
+
 end
