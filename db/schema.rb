bba532b9663de1dbd36f383756a11e2b56819146
Saad <Dropheaven@live.com>
2017-06-15 07:33:14 -0400

    added user_id column to bills table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170614181343) do
+ActiveRecord::Schema.define(version: 20170614203525) do
 
   create_table "bills", force: :cascade do |t|
     t.string "name"
     t.decimal "amount"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
